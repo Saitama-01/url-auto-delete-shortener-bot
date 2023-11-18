1231bf409cf1b16c9bc6b75d940a0e90943f2309
Saitama-01 <145284677+Saitama-01@users.noreply.github.com>
2023-11-19 02:17:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+API_ID = int(environ.get('API_ID', '12333972'))
+API_HASH = environ.get('API_HASH', 'a0f38ec8c608cc7011f563661580bdfe')
+BOT_TOKEN = environ.get('BOT_TOKEN', "6849923132:AAH053hdhTEdn8tY9Jkm0KqTleG_EBLQnQ4")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
